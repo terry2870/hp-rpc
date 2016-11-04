20c3c430388c66bd832b1bf68528145bebe04e8c
ping <terry2870@163.com>
2016-11-04 19:53:18 +0800

    update

---
+++ server/src/main/java/com/hp/rpc/server/RPCServer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.hp.rpc.server;
+
+import java.util.List;
+
+/**
+ * 远程调用服务端
+ * @author ping.huang
+ * 2016年11月4日
+ */
+public class RPCServer {
+
+	/**
+	 * 注册到zk的地址
+	 */
+	private String zkAddress;
+	
+	private List<Object> 
+	
+}
